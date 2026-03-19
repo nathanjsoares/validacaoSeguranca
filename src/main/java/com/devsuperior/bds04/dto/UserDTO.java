e71b5239fd9e84afc33fee6c015894e1127f02bf
Nathan Soares <nathansoares@email.com>
2026-03-19 15:27:03 -0300

    First Commit

---
+++ src/main/java/com/devsuperior/bds04/dto/UserDTO.java
@@ -0,0 +1,66 @@
+package com.devsuperior.bds04.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.bds04.entities.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserDTO {
+
+	private Long id;
+	
+	@NotBlank(message = "Campo obrigatório")
+	private String name;
+
+	@Email(message = "Favor entrar um email válido")
+	private String email;
+	
+	Set<RoleDTO> roles = new HashSet<>();
+	
+	public UserDTO() {
+	}
+
+	public UserDTO(Long id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	public UserDTO(User entity) {
+		id = entity.getId();
+		name = entity.getName();
+		email = entity.getEmail();
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+}
---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new CityDTO(x)).toList();
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+
+    private void copyDtoToEntity(CityDTO dto, City entity) {
+        entity.setName(dto.getName());
+    }
+}
